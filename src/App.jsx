301f1c5038ccd2fec6cc94507133d0045904ace8
mohdshoaibkhan72 <mohdshoaibkhan7211@gmail.com>
2025-01-21 16:31:55 +0530

    refactor: add About Us section with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/HeroSections/Hero";
+import Carts from "./Components/Aboutus/Aboutus";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Carts></Carts>
     </>
   );
 }
---
+++ src/Components/Aboutus/Aboutus.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Aboutus.css"; // Import the CSS file
+
+function Aboutus() {
+  return (
+    <div className="about-container">
+      {/* About Us Section */}
+      <div className="about-section">
+        <div className="about-text">
+          <h1>About Us</h1>
+          <h2>Welcome to Our Tea Store</h2>
+          <p>
+            We are a family-run company founded on a passion for tea. Our
+            Chairman made a promise to never sell tea he wouldn’t drink at home.
+            Learn how we stay true to those values in everything we do.
+          </p>
+          <button className="learn-more-button">Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Aboutus;
